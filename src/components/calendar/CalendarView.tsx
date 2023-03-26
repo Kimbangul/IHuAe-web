74db470d9253ec4505d8400a109eb01a30f73633
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-03-27 00:17:23 +0900

    style: common header component add

---
+++ src/components/calendar/CalendarView.tsx
@@ -0,0 +1,11 @@
+import Header from '../common/Header';
+
+const CalendarView = () => {
+  return (
+    <>
+      <Header title='캘린더' />
+    </>
+  );
+};
+
+export default CalendarView;
--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,10 +1,11 @@
 import PageWrapper from '@/components/common/PageWrapper';
+import CalendarView from '@/components/calendar/CalendarView';
 
 export default function Calendar() {
   return (
     <>
       <PageWrapper>
-        <>calendar</>
+        <CalendarView />
       </PageWrapper>
     </>
   );
