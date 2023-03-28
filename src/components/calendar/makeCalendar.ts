3809d249b1799395ead3a835a4f6de9209d24238
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-03-28 21:21:03 +0900

    test: test case add

--- src/components/calendar/makeCalendar.ts
+++ src/components/calendar/makeCalendar.ts
@@ -1,19 +1,54 @@
 import moment from 'moment';
 import leftPad from '@/utils/leftPad';
+import { current } from '@reduxjs/toolkit';
+
+// CLASS 날짜 정보 생성자
+class DateInfo {
+  date: moment.Moment;
+  currentMonth: boolean;
+
+  constructor(date: moment.Moment, currentMonth: boolean) {
+    this.date = date;
+    this.currentMonth = currentMonth;
+  }
+}
 
 const makeMonthCalendar = (year: number, month: number) => {
   // PARAM 이번 달 마지막 날짜
   const lastDate = moment().endOf('month'); // .format('DD')
   // PARAM 이번 달 첫번째 날짜 요일
-  const firstDay = moment().startOf('month').day();
+  const firstDay = moment().startOf('month'); //.day();
 
   const calenderArr = [];
   let weekArr = Array(7).fill('');
-  let dayIndex = firstDay;
+  let dayIndex = firstDay.day();
 
   // 이전 달 날짜 출력
   if (dayIndex !== 0) {
-    let lastMonthLastDate = lastDate.subtract('days', 1);
+    let lastMonthLastDate = firstDay.subtract('days', 1);
+
+    for (let i = dayIndex - 1; i >= 0; i--) {
+      weekArr[i] = new DateInfo(lastMonthLastDate, false);
+    }
+
+    lastMonthLastDate = lastMonthLastDate.subtract('days', 1);
+  }
+
+  // 이번 달 날짜 출력
+  for (let date = 1; date < Number(lastDate.format('DD')); date++) {
+    let day = firstDay.add('days', date);
+    weekArr[dayIndex] = new DateInfo(day, false);
+
+    if (dayIndex === 6) {
+      break;
+    }
+    // 마지막 인덱스일 경우
+    if (date === lastDate.day()) {
+      for (let i = lastDate.day() + 1; i <= 6; i++) {
+        let nextMonthDate = lastDate.add('days', i);
+        weekArr[dayIndex] = new DateInfo(nextMonthDate, false);
+      }
+    }
   }
 };
 
---
+++ src/components/calendar/test.ts
@@ -0,0 +1,47 @@
+import moment from 'moment';
+
+// FUNCTION 이번 달 마지막 날짜
+test('last date', () => {
+  const lastDate = moment().endOf('month');
+  expect(lastDate.format).toBe(moment(new Date(2023, 2 + 1, 0)).format);
+});
+
+// FUNCTION 이번 달 첫번째 날짜 요일
+test('first Day', () => {
+  const firstDay = moment().startOf('month').day();
+  const firstDay2 = new Date(2023, 2, 1).getDay();
+
+  expect(firstDay).toBe(firstDay2);
+});
+
+// FUNCTION 이전 달 마지막 날짜
+// test('lat date of last month', () => {
+//   const firstDay = moment().startOf('month');
+//   const lastMonthLastDate = firstDay.subtract('days', 1);
+//   const lastMonthLastDate2 = String(new Date(2023, 1, 28));
+
+//   expect(lastMonthLastDate2).toBe(lastMonthLastDate);
+// });
+
+// FUNCTION 타입 체크
+test('get moment type', () => {
+  let lastMonthLastDate = moment().startOf('month').subtract('days', 1);
+
+  expect(typeof lastMonthLastDate).toBe('object');
+});
+
+// FUNCTION 날짜 빼기 체크
+test('check subtract', () => {
+  const beforeTwoDay = moment().subtract('days', 1).subtract('days', 1);
+  const beforeTwoDay2 = moment().subtract('days', 2);
+
+  expect(beforeTwoDay).toStrictEqual(beforeTwoDay2);
+});
+
+// 날짜 변환 체크
+test('check subtract', () => {
+  const lastDate = moment().endOf('month');
+  const lastDateToString = Number(lastDate.format('DD'));
+
+  expect(lastDateToString).toBe(31);
+});
