c9234bde6a5bcc7f16e176c261e30098bee7febc
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2023-04-04 19:14:46 +0900

    style: text 변경

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -1,42 +1,51 @@
-import { useEffect, useState, useMemo } from "react";
-import ChatView from "./ChatView";
-import { ChatBubble } from "./ChatType";
-import moment from "moment";
+import { useEffect, useState, useMemo } from 'react';
+import ChatView from './ChatView';
+import { ChatBubble } from './ChatType';
+import moment from 'moment';
 
 const ChatContainer = () => {
-
   // PARAM state
   const [data, setData] = useState<ChatBubble[]>([]);
   const [inputVal, setInputVal] = useState('');
   const [isMore, setIsMore] = useState(false);
 
   // PARAM sub header text
-  const subHeaderText = useMemo(()=>{
-    return isMore ? '열림 상태 열림 상태 열림 상태' : '닫힘 상태';
+  const subHeaderText = useMemo(() => {
+    return isMore
+      ? '이 곳은 충동적인 감정을 억제하기 위한 곳입니다.'
+      : '이 곳은 충동적인 감정을 억제하기 위한 곳입니다.\n상대방에게 하고 싶은 말이 있다면, 이 곳에 써주세요.\n하루가 지나면, 당신의 말들은 사라집니다.';
   }, [isMore]);
 
   // FUNCTION 메세지 전송
-  const sendMsg = (e: React.KeyboardEvent<HTMLTextAreaElement>) =>{   
+  const sendMsg = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key !== 'Enter') return;
     e.preventDefault();
     const newMsg = new ChatBubble(moment(), inputVal);
     setData((prev) => [...prev, newMsg]);
-    setInputVal('');    
-  }   
+    setInputVal('');
+  };
 
   // FUNCTION scroll to bottom
-  useEffect(()=>{
-    if(data.length <= 0) return;
+  useEffect(() => {
+    if (data.length <= 0) return;
     const chatContent = document.querySelector('#chartContent');
     if (!(chatContent instanceof HTMLElement)) return;
     chatContent.scrollTop = chatContent.scrollHeight;
   }, [data]);
 
-  return(
+  return (
     <>
-      <ChatView list={data} sendMsg={sendMsg} isMore={isMore} setIsMore={setIsMore} inputVal={inputVal} setInputVal={setInputVal} subHeaderText={subHeaderText}/>
+      <ChatView
+        list={data}
+        sendMsg={sendMsg}
+        isMore={isMore}
+        setIsMore={setIsMore}
+        inputVal={inputVal}
+        setInputVal={setInputVal}
+        subHeaderText={subHeaderText}
+      />
     </>
-  )
-}
+  );
+};
 
 export default ChatContainer;
--- src/components/chat/ChatView.tsx
+++ src/components/chat/ChatView.tsx
@@ -1,34 +1,37 @@
 import Header from '@/components/common/Header';
-import Chat, {SubHeader} from './ChatStyle';
-import BubbleView from "./BubbleView";
+import Chat, { SubHeader } from './ChatStyle';
+import BubbleView from './BubbleView';
 import { ChatViewPropsType } from './ChatType';
 import SUBHEADER_CHEVRON_14 from '@/assets/icons/chat/subheader_chevron_14.svg';
 
-const ChatView = ({list, sendMsg, setInputVal, inputVal, subHeaderText, isMore, setIsMore} :  ChatViewPropsType) => {
-  return(
+const ChatView = ({ list, sendMsg, setInputVal, inputVal, subHeaderText, isMore, setIsMore }: ChatViewPropsType) => {
+  return (
     <Chat.Container>
-      <Header title='header' back={true}/>
+      <Header title='감정 억제기' back={true} />
       <SubHeader.Container>
         <SubHeader.Desc>{subHeaderText}</SubHeader.Desc>
-        <SubHeader.Button isMore={isMore} onClick={()=>setIsMore(!isMore)}>
+        <SubHeader.Button isMore={isMore} onClick={() => setIsMore(!isMore)}>
           <SUBHEADER_CHEVRON_14 />
-        </SubHeader.Button>        
+        </SubHeader.Button>
       </SubHeader.Container>
       <Chat.Content id='chartContent'>
-        <Chat.Desc>desc</Chat.Desc>
+        <Chat.Desc>
+          이 곳은 충동적인 감정을 억제하기 위한 곳입니다.
+          <br />
+          상대방에게 하고 싶은 말이 있다면, 이 곳에 써주세요. <br />
+          하루가 지나면, 당신의 말들은 사라집니다.
+        </Chat.Desc>
         <Chat.ChatList>
-          {
-            list.map((el, idx) => {
-              return <BubbleView content={el.message} key={`message${idx}`}/>
-            })
-          }   
-        </Chat.ChatList>        
+          {list.map((el, idx) => {
+            return <BubbleView content={el.message} key={`message${idx}`} />;
+          })}
+        </Chat.ChatList>
       </Chat.Content>
       <Chat.Input.Container>
-        <Chat.Input.Input value={inputVal} onChange={(e) => setInputVal(e.target.value)} onKeyPress={sendMsg}/>
+        <Chat.Input.Input value={inputVal} onChange={(e) => setInputVal(e.target.value)} onKeyPress={sendMsg} />
       </Chat.Input.Container>
     </Chat.Container>
-  )
-}
+  );
+};
 
 export default ChatView;
